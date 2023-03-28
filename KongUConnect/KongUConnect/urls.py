4478c11c1a05813d7f72fe3ff2029244f9bcb48a
HannahHK0622 <kanyiuyeung@gmail.com>
2023-03-28 22:04:26 +0800

    Final changes for the day.

--- KongUConnect/KongUConnect/urls.py
+++ KongUConnect/KongUConnect/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('CoreUI.urls')),
+    # path('Timetable', include('TimetableMgr.urls')),
+    # path('Venues', include('VenueMgr.urls'))
 ]
